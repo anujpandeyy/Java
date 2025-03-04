2c5e8b5394bebae6b7b33bf8bba6eed55c945017
Anuj Pandey <anujpandey1701@yahoo.com>
2025-03-04 23:55:42 +0530

    sc

--- java_home/Main.java
+++ java_home/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st num:");
+        int n1 = sc.nextInt();
+        System.out.println("Enter 2nd num:");
+        int n2 = sc.nextInt();
+        
+        float sum = n1+n2;
+
+        System.out.println(sum);
+        
     }
 }
