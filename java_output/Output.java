4baaef065f7eb13830b39f9cc3c031ba598ada30
Anuj Pandey <anujpandey1701@yahoo.com>
2025-03-05 22:00:38 +0530

    Java Output

---
+++ java_output/Output.java
@@ -0,0 +1,10 @@
+package java_output;
+public class Output{
+    public static void main(String[] args){
+        System.out.println("Anuj Pandey");
+        System.out.println(22);
+        System.out.println(25+5);
+        System.out.print("I am learning Java ");
+        System.out.println("and it's fun!");
+    } 
+}
