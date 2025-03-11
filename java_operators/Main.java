50a66b0de5bb43d1fa79bb2cd860bac4175b43cb
Anuj Pandey <anujpandey1701@yahoo.com>
2025-03-11 22:43:32 +0530

    Java Operators

---
+++ java_operators/Main.java
@@ -0,0 +1,13 @@
+package java_operators;
+
+public class Main {
+    public static void main(String[]args){
+        int a = 15;
+        int b = 4;
+        System.out.println(a/b);
+        System.out.println(a%b);
+        int newI = a+=b;
+        System.out.println(newI);
+        System.out.println(a>b);
+    }
+}
