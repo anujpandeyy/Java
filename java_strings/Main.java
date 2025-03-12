4332ec632f0013761170772e023b652c6c725558
Anuj Pandey <anujpandey1701@yahoo.com>
2025-03-12 23:30:14 +0530

    Java Strings

---
+++ java_strings/Main.java
@@ -0,0 +1,12 @@
+package java_strings;
+
+public class Main {
+    public static void main(String[] args){
+        String fname = "Anuj ";
+        String lname = "Pandey";
+        System.out.println(fname.toUpperCase());
+        System.out.println(fname.indexOf(" "));
+        System.out.println(fname+lname);
+        System.out.println(fname.concat(lname));
+    }
+}
