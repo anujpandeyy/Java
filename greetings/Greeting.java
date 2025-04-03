3736da1f128db8ff0712b125a2bbbf583ce6dcee
Anuj Pandey <anujpandey1701@yahoo.com>
2025-04-03 22:03:37 +0530

    Java Packages

---
+++ greetings/Greeting.java
@@ -0,0 +1,7 @@
+package greetings;
+
+public class Greeting {
+    public String sayHello() {
+        return "Hello from the greetings package!";
+    }
+}
---
+++ java_packages/Main.java
@@ -0,0 +1,10 @@
+package java_packages;
+
+import greetings.Greeting;
+
+public class Main {
+    public static void main(String[] args) {
+        Greeting obj = new Greeting();
+        System.out.println(obj.sayHello());
+    }
+}
