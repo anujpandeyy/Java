88bc7df298558166701f3feeca741f427a7566a2
Anuj Pandey <anujpandey1701@yahoo.com>
2025-04-08 23:54:04 +0530

    Java Interface

---
+++ java_interface/Main.java
@@ -0,0 +1,62 @@
+package java_interface;
+
+
+interface Shape {
+    double area();
+    double perimeter();
+}
+
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+
+class Rectangle implements Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5); 
+        Shape rectangle = new Rectangle(4, 6); 
+
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Circle Perimeter: " + circle.perimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.area());
+        System.out.println("Rectangle Perimeter: " + rectangle.perimeter());
+    }
+}
+
